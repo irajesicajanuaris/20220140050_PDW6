eaca986de0ed923a2f9064e514cce762d9b0bf50
irajesicajanuaris <jjiraaa1@gmail.com>
2024-04-07 13:15:24 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+  alert("ini video Better Things");
+});
+
+audio.addEventListener("click", function audio() {
+  alert("ini audio Better Things");
+});
+
+gambar.addEventListener("click", function audio() {
+  alert("ini gambar Better Things");
+});
